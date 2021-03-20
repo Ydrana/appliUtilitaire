3dfe05242cf8fdcfaa8ee82556ea606896e11b2e
Moth <moth89@hotmail.fr>
2021-03-20 16:19:23 +0100

    Ajout installation visualiseur

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # coding: utf-8
-import incrementeurDate, TraqueMesCles
+import incrementeurDate
+import TraqueMesCles
 from tkinter import Label, Frame, Button
 from tkinter import Label as tkLabel
 from ttkthemes import ThemedTk
 
+import requests
+from github import Github
+from pathlib import Path
+import subprocess
 
-version: str = '0.1'
+__version__: str = '0.1.0'
+
 
 def ouvrir_traque_mes_cles():
     print('Ouvrir CheckMesClés')
@@ -24,6 +30,29 @@ def callback_to_principal():
     frame_incrementeur_date.grid_remove()
     frame_traque_mes_cles.grid_remove()
     frame_principale.grid()
+
+
+def installer_visualiseur():
+    """Télécharge de Github l'asset de la dernière release du visualiseur d'offres"""
+    try:
+        import github_id
+
+        headers = {'Authorization': 'token ' + github_id.token,
+                   'Accept': 'application/octet-stream'}
+
+        gh_session = requests.Session()
+        gh_session.auth = (github_id.user, github_id.token)
+
+        g = Github(github_id.token)
+        repo = g.get_repo("Ydrana/Visualiseur_de_donnees")
+
+        asset = repo.get_latest_release().get_assets()[1]
+        response = gh_session.get(asset.url, stream=True, headers=headers)
+        save_to = Path.home() / 'Downloads' / asset.name
+        save_to.write_bytes(response.content)
+        subprocess.Popen([save_to], shell=True)
+    except Exception:
+        print("Fichier github_id absent")
 
 
 if __name__ == '__main__':
@@ -46,15 +75,17 @@ if __name__ == '__main__':
 
     label_fonct = Label(frame_principale, text='Fonctionnel')
     bouton_lancer = Button(frame_principale, text="Incrémenter dates XML", command=ouvrir_incrementeur_date)
+    bouton_dl_visualiseur = Button(frame_principale, text="installation visualiseur d'offres", command=installer_visualiseur)
     label_java = Label(frame_principale, text='Code Java')
     bouton_lancer_traque = Button(frame_principale, text="Vérifier clés messages", command=ouvrir_traque_mes_cles)
-    label_version = tkLabel(frame_principale, text="v"+version, fg="#797979", font=('Arial', 8), anchor='se')
+    label_version = tkLabel(frame_principale, text="v" + __version__, fg="#797979", font=('Arial', 8), anchor='se')
 
     label_fonct.grid(row=0, column=0, pady=(15, 5))
     bouton_lancer.grid(row=1, column=0)
-    label_java.grid(row=2, column=0, pady=(15, 5))
-    bouton_lancer_traque.grid(row=3, column=0)
-    label_version.grid(row=4, column=0, sticky='WE')
+    bouton_dl_visualiseur.grid(row=2, column=0)
+    label_java.grid(row=3, column=0, pady=(15, 5))
+    bouton_lancer_traque.grid(row=4, column=0)
+    label_version.grid(row=5, column=0, sticky='WE')
 
     # FRAME Incrementeur de date (removed au démarrage)
     frame_incrementeur_date = Frame(window)
@@ -69,4 +100,3 @@ if __name__ == '__main__':
     app_traque_mes_cles = TraqueMesCles.TraqueMesCles(frame_traque_mes_cles, callback_to_principal)
 
     window.mainloop()
-
